ff57c534518ca885e86ca2c0ff8464b4e8056765
betterin30days <leethomascostello@gmail.com>
2018-09-18 19:39:20 -0400

    initial parsing grammar and logic

--- parsing.c
+++ parsing.c
@@ -32,6 +32,23 @@
 #endif
 
 int main(int argc, char** argv) {
+  /* create some parsers */
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Leesp = mpc_new("leesp");
+
+  /* define them with the following language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+    " \
+      number: /-?[0-9]+/ ; \
+      operator: '+' | '-' | '*' | '/' ; \
+      expr: <number> | '(' <operator> <expr>+ ')' ; \
+      leesp: /^/ <operator> <expr>+ /$/ ; \
+    ",
+    Number, Operator, Expr, Leesp
+  );
+
   puts("Leesp version 0.0.0.0.1");
   puts("Press ctrl+c to exit\n");
 
@@ -39,9 +56,24 @@ int main(int argc, char** argv) {
     char* input = readline("leesp> ");
     add_history(input);
 
-    printf("You said %s\n", input);
+    /* attempt to parse the user input */
+    mpc_result_t r;
+    // mpc_parse returns 1 on success and 0 on failure
+    if (mpc_parse("<stdin>", input, Leesp, &r)) {
+      /* on success print the AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* otherwise print the error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
     free(input);
   }
+
+  /* undefine and delete our parsers */
+  mpc_cleanup(4, Number, Operator, Expr, Leesp);
 
   return 0;
 }
