ff90d14462614fbbc9a23af889b5570eb7c7df59
betterin30days <leethomascostello@gmail.com>
2018-10-20 21:00:54 -0400

    add built in if function for control flow

--- main.c
+++ main.c
@@ -71,6 +71,7 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, "!=", builtin_not_equal);
 
   lenv_add_builtin(e, "\\", builtin_lambda);
+  lenv_add_builtin(e, "if", builtin_if);
 }
 
 int main(int argc, char** argv) {
